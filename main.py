c54e4d0e6fea003201a8c751e9454962304ad90a
Code_Deev <chic11951@gmail.com>
2024-10-20 22:03:41 +0100

    feat: implemented high score

--- main.py
+++ main.py
@@ -39,16 +39,16 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     # [1:] is a way of slicing and will return all segments except the first one. this way of slicing works for both lists and tuples
     for segment in snake.segments[1:]:
         # This line checks if the distance between the snake's head and the current segment is less than 10 units.
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 
